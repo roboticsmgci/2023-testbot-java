b6d370eafbd54740f3f61f6854ada093977d4462
Lucas Kim <lucaskamadakim@gmail.com>
2023-03-13 13:09:48 -0400

    Reset angle in Turn.java

--- src/main/java/frc/robot/commands/Turn.java
+++ src/main/java/frc/robot/commands/Turn.java
@@ -4,24 +4,30 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Drivetrain;
 
 public class Turn extends CommandBase {
-    private double m_degrees;
+    private final double m_degrees;
+    public double m_targetAngle;
     private final Drivetrain m_drivetrain;
 
-    private final double allowedError = 10;
+    private final double allowedError = 2;
 
     public Turn(double degrees, Drivetrain drivetrain) {
         m_drivetrain = drivetrain;
-        m_degrees = m_drivetrain.m_navX.getAngle() + degrees;
+        m_degrees = degrees;
         
         setName("Turn");
         addRequirements(m_drivetrain);
     }
 
     @Override
+    public void initialize() {
+        m_targetAngle = m_drivetrain.m_navX.getAngle() + m_degrees;
+    }
+
+    @Override
     public void execute() {
         double currentDegrees = m_drivetrain.m_navX.getAngle();
 
-        if (currentDegrees < m_degrees) {
+        if (currentDegrees < m_targetAngle) {
             m_drivetrain.drive(0.5, -0.5);
         } else {
             m_drivetrain.drive(-0.5, 0.5);
@@ -32,7 +38,7 @@ public class Turn extends CommandBase {
     public boolean isFinished() {
         double currentDegrees = m_drivetrain.m_navX.getAngle();
 
-        return Math.abs(currentDegrees - m_degrees) < allowedError;
+        return Math.abs(currentDegrees - m_targetAngle) < allowedError;
     }
   
     // Called once after isFinished returns true
