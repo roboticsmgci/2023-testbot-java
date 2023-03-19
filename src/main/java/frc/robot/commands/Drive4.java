1f5e820f8a65c873d4a55137a3250487477cc05d
Lucas Kim <lucaskamadakim@gmail.com>
2023-03-19 08:46:51 -0400

    Fix some pid stuff in Drive4

--- src/main/java/frc/robot/commands/Drive4.java
+++ src/main/java/frc/robot/commands/Drive4.java
@@ -13,6 +13,8 @@ import frc.robot.subsystems.Drivetrain;
 
 public class Drive4 extends CommandBase {
     private Joystick m_xbox;
+    private double m_initialAngle;
+
     //private double distance;
     private final double kp = 0.008;
     private final double ki = 0;
@@ -32,23 +34,24 @@ public class Drive4 extends CommandBase {
 
     @Override
     public void initialize(){
+        m_initialAngle = m_drivetrain.m_navX.getAngle();
     }
 
     @Override
     public void execute() {
         if(m_xbox.getRawButton(5)&&m_xbox.getRawButton(6)){
-            m_drivetrain.m_navX.reset();
+            m_initialAngle = m_drivetrain.m_navX.getAngle();
         }
 
         double speed = -0.7*m_xbox.getRawAxis(1);//Math.min(1, Math.hypot(m_xbox.getRawAxis(0), m_xbox.getRawAxis(1)));//filter.calculate(0.9*Math.min(1, m_xbox.getMagnitude()));//Math.hypot(x, y)
 
-        double angle = Math.toDegrees(Math.atan2(m_xbox.getRawAxis(2), -m_xbox.getRawAxis(3)));//Math.atan(x, -y)
+        double angle = Math.toDegrees(Math.atan2(m_xbox.getRawAxis(4), -m_xbox.getRawAxis(5)));//Math.atan(x, -y)
 
         m_drivetrain.angle = angle;
 
         double currentGyro = m_drivetrain.m_navX.getAngle();
 
-        double correction = MathUtil.clamp(pid.calculate(currentGyro, currentGyro + angle), -0.3, 0.3);
+        double correction = MathUtil.clamp(pid.calculate(currentGyro, angle), -0.3, 0.3);
 
         if (Math.hypot(m_xbox.getRawAxis(4), m_xbox.getRawAxis(5)) < 0.1) {
             correction = 0;
